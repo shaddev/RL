1e21e04a87334e712cede572fe10f8710e7769c9
Shad Ahmed <shadahmed2015@gmail.com>
2025-04-19 22:08:56 -0500

    ppo eval

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 class MLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -101,10 +102,89 @@ def mlp_train(use_saved = False):
 
 def mlp_eval():
 
+    states = torch.tensor(df[observation_cols].values, dtype=torch.float32)
+    actions = torch.tensor(df["a:action"].values, dtype=torch.int64)
+
+    X_train, X_test, y_train, y_test = train_test_split(states, actions, test_size=0.2, random_state=42)
+
+    model = torch.load("mlp.pth")
     model.eval()
-    y = torch.argmax(model(X_test), dim=-1)
-    print(torch.sum(y == y_test) / len(y_test))
+    y = torch.argmax(model(states), dim=-1)
+    print(torch.sum(y == actions) / len(actions))
     print(y[:200])
+
+
+
+
+def ppo_eval():
+
+    # X_train, X_test, y_train, y_test = train_test_split(states, actions, test_size=0.2, random_state=42)
+
+    # KNN for behavior policy
+    # knn = KNeighborsClassifier(n_neighbors=5)
+    # knn.fit(X_train, y_train)
+
+    C1 = -0.125
+    C2 = -2
+    terminal_rewards = torch.tensor(df["r:reward"].values, dtype=torch.float32)
+    dones = torch.tensor(df["r:reward"].values != 0, dtype=torch.float32)
+    r_states = torch.tensor(df[["o:SOFA","o:Arterial_lactate", "traj", "step"]].values, dtype=torch.float32)
+    next_states = torch.cat((torch.zeros(1, 4), r_states[:-1]))
+    rewards = C1 * (next_states[:, 0] - r_states[:, 0]) + C2 * np.tanh(next_states[:, 1] - r_states[:, 1])
+    rewards[dones == 1] = 15 * terminal_rewards[dones == 1]
+    
+    mlp = torch.load("mlp.pth").eval()
+    ppo_actor = torch.load("ppo_actor.pth").eval()
+
+    mlp_out = mlp(states)
+    ppo_actor_out = ppo_actor(states)
+
+    mlp_probs = F.softmax(mlp_out, dim=1)
+    mlp_probs = mlp_probs.gather(1, actions.unsqueeze(1)).squeeze(1)
+
+    ppo_actor_probs = F.softmax(ppo_actor_out, dim=1)
+    ppo_actor_probs = ppo_actor_probs.gather(1, actions.unsqueeze(1)).squeeze(1)
+
+    print(mlp_probs.shape)
+
+    rho_all = (ppo_actor_probs / (mlp_probs + 1e-8)).detach().numpy()
+
+    max_timesteps = df['step'].max() + 1
+
+    rho = []
+    all_rewards = []
+    cur_step = 0
+    for traj_id, group in df.groupby('traj'):
+
+        rew = rewards[cur_step : cur_step + len(group)]
+        rew = np.pad(rew, (0, max_timesteps - len(group)), mode='constant')
+        all_rewards.append(rew)
+
+        rho_group = rho_all[cur_step : cur_step + len(group)]
+        rho_group = np.pad(rho_group, (0, max_timesteps - len(group)), mode='constant')
+        rho.append(rho_group)
+
+        cur_step += len(group)
+    
+    rho = np.stack(rho)
+    all_rewards = np.stack(all_rewards)
+
+    # print(mlp_probs)
+
+
+
+    # # Compute cumulative product of importance weights per time step
+    w = torch.cumprod(torch.tensor(rho), dim=1)  # shape [N, H]
+
+    # # Weighted sum per time step
+    numerator = (w * all_rewards).sum(dim=0)     # shape [H]
+    denominator = w.sum(dim=0) + 1e-8        # avoid div-by-zero
+
+    phwis = (numerator / denominator).sum()  # scalar estimate
+
+    print(f"final score = {phwis}")
+
+    
 
 
 def lstm_train(use_saved = False):
@@ -143,17 +223,18 @@ def lstm_train(use_saved = False):
     return model
 
 def ppo_train():
-    C1 = -1
+    C1 = -0.125
     C2 = -2
     gamma = 0.9
     epsilon = 0.2
     lr = 0.0001
+    weight_decay = 0.00001
 
     lstm = lstm_train(True)
     actor = mlp_train(True)
     critic = MLP(input_dim, hidden_dim, 1)
-    actor_optim = optim.Adam(actor.parameters(), lr=lr)
-    critic_optim = optim.Adam(critic.parameters(), lr=lr)
+    actor_optim = optim.Adam(actor.parameters(), lr=lr, weight_decay=weight_decay)
+    critic_optim = optim.Adam(critic.parameters(), lr=lr, weight_decay=weight_decay)
 
     lstm.eval()
     #actor_model.eval()
@@ -163,7 +244,7 @@ def ppo_train():
     sofa_index = observation_cols.index('o:SOFA')
     lactate_index = observation_cols.index('o:Arterial_lactate')
 
-    epochs = 100
+    epochs = 250
     trajs_per_epoch = 500
     timesteps = 10
     num_iters = 5
@@ -187,7 +268,7 @@ def ppo_train():
             current_timestep = trajs[:,-1,:-1] # get all observation features (i.e. everything except action)
             actor_out = actor(current_timestep)
             actions = torch.argmax(actor_out, dim=-1).unsqueeze(1)
-            next_timestep = lstm(trajs[:, [timestep, timestep+1, timestep+2], :])
+            next_timestep = lstm(trajs[:, [timestep, timestep+1, timestep+2], :]).detach()
             next_timestep = torch.cat([next_timestep, actions], dim=1).unsqueeze(1)
             trajs = torch.cat([trajs, next_timestep], dim=1)
 
@@ -200,7 +281,7 @@ def ppo_train():
             # print(next_timestep.shape)
 
             reward = (C1 * (next_timestep[:, 0, sofa_index] - current_timestep[:, sofa_index]) \
-                    + C2 * (next_timestep[:, 0, lactate_index] - current_timestep[:, lactate_index]))
+                    + C2 * np.tanh(next_timestep[:, 0, lactate_index] - current_timestep[:, lactate_index]))
             
             batch_rewards.append(reward)
         #print(torch.stack(batch_action_probs).T.shape)
@@ -227,8 +308,6 @@ def ppo_train():
         A = (batch_rewards - critic(batch_obs).squeeze()).detach()
 
         critic_criterion = nn.MSELoss()
-
-        torch.autograd.set_detect_anomaly(True)
             
         for i in range(num_iters):
 
@@ -263,7 +342,9 @@ def ppo_train():
 def main():
     #mlp_train()
     #lstm_train()
-    ppo_train()
+    #ppo_train()
+    #mlp_eval()
+    ppo_eval()
 
 main()
     
